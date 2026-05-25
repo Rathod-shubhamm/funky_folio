42e0f448dd2f636de06903fc0212d09ab24f4dae
Shubham Rathod <shubhamrathod1619@gmail.com>
2026-05-26 01:30:01 +0530

    resume link updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
             <span>LinkedIn</span>
             <span aria-hidden="true">LinkedIn</span>
           </a>
-          <a href="https://drive.google.com/file/d/1fQXOz1VRL4lXThWAij_WKyUHa8Ltr4Ke/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <a href="https://drive.google.com/file/d/17De1K-upPAE7M0uE8Q8ixuyyaNDv5PPr/view?usp=sharing" target="_blank" rel="noopener noreferrer">
             <span>Resume</span>
             <span aria-hidden="true">Resume</span>
           </a>
